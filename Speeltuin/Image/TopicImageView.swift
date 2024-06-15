3e10171934c7023d0061b3da96235b06955c39fe
Ivana Kolin <cznlde@proton.me>
2024-06-15 13:33:17 +0200

    zoom image on tab gesture

--- Speeltuin/Image/TopicImageView.swift
+++ Speeltuin/Image/TopicImageView.swift
@@ -7,10 +7,11 @@
 import SwiftUI
 
 struct TopicImageView: View {
-    let topic = Topic(name: "Images", description: "Image, Async Image", image: "photo")
-
+    let topic = Topic(name: "Images", description: "Image, Async Image, Tap, Zoom", image: "photo")
+    @State private var zoom = false
     var body: some View {
         BaseTopicView(topic: topic) {
+        
             VStack {
                 Image(systemName: "star.fill")
                     .resizable()
@@ -27,7 +28,11 @@ struct TopicImageView: View {
                     .shadow(radius: 10)
                 AsyncImage(url: URL(string: "https://picsum.photos/200")!) { image in
                     image.resizable()
-                        .aspectRatio(contentMode: .fit)
+                        .aspectRatio(contentMode: zoom ? .fill : .fit)
+                        .onTapGesture {
+                            withAnimation { zoom.toggle()
+                            }
+                        }
                 } placeholder: {
                     ProgressView("Loading...")
                 }
