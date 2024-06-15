aa3649c3dd60fd00e4a379450b4ff0d66c1553d7
Ivana Kolin <cznlde@proton.me>
2024-06-15 09:06:15 +0200

    Initial Commit

---
+++ Speeltuin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Speeltuin
+//
+//  Created by Ivana Kolin on 15.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Speeltuin/SpeeltuinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeeltuinApp.swift
+//  Speeltuin
+//
+//  Created by Ivana Kolin on 15.06.24.
+//
+
+import SwiftUI
+
+@main
+struct SpeeltuinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
