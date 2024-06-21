b465def1300e769c796f807beeea3a5eb7de86d4
Ivana Kolin <cznlde@proton.me>
2024-06-21 15:49:27 +0200

    Create Config.swift
    Add placeholder Config.swift with API key comment

---
+++ Speeltuin/Config.swift
@@ -0,0 +1,18 @@
+//
+//  Config.swift
+//  Speeltuin
+//
+//  Created by Ivana Kolin on 16.06.24.
+//
+
+ 
+import Foundation
+
+struct Config {
+    // Add your OpenWeather API key here.
+    // Replace 'YOUR_API_KEY_HERE' with your actual API key.
+    // This key is necessary to authenticate requests to the OpenWeather API.
+    // For more information on how to get your API key, visit:
+    // https://home.openweathermap.org/users/sign_up
+    static let OpenWeaterAppApiKey = "YOUR_API_KEY"
+}
