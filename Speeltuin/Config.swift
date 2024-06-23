8eaebf4384b701cb14ac0f6dde4b6e8817ee85dd
Ivana Kolin <cznlde@proton.me>
2024-06-23 08:50:59 +0200

    add openAIAppApiKey

--- Speeltuin/Config.swift
+++ Speeltuin/Config.swift
@@ -5,8 +5,14 @@
 //  Created by Ivana Kolin on 16.06.24.
 //
 
- 
+
 import Foundation
+
+// om wijzigingen niet meer in github pushen
+// git update-index --assume-unchanged Config.swift
+
+// om dit bestand te kunnen comitten
+// git update-index --no-assume-unchanged Config.swift
 
 struct Config {
     // Add your OpenWeather API key here.
@@ -14,5 +20,6 @@ struct Config {
     // This key is necessary to authenticate requests to the OpenWeather API.
     // For more information on how to get your API key, visit:
     // https://home.openweathermap.org/users/sign_up
-    static let OpenWeaterAppApiKey = "YOUR_API_KEY"
+    static let OpenWeaterAppApiKey = "YOUR_API_KEY_HERE"
+    static let OpenAIAppApiKey = "YOUR_API_KEY_HERE"
 }
